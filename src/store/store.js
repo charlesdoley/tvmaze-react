7ba27e69df92056e4ad201b5c3e48a3b3a0d938b
Charlis <charles.doley1@gmail.com>
2020-10-07 08:54:37 +0530

    TV Maze- React - Updated redux tools in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,10 @@
-import {createStore, compose, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
+
 import thunk from 'redux-thunk';
 import reducer from './reducers/index';
 
-const allEnhancers = compose (
-  applyMiddleware (thunk),
-  window.devToolsExtension && window.devToolsExtension ()
-);
+const allEnhancers = composeWithDevTools(applyMiddleware(thunk))
 
 const store = createStore (reducer, allEnhancers);
 export default store;
